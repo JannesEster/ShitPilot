39ec621250717572e5821590d757829a713cfa45
JannesEster <104826287+JannesEster@users.noreply.github.com>
2023-07-04 14:19:55 +0800

    Added more useability and styling

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 function andGo() {
    
-
+    
+if (document.querySelector(".onOff").checked) {
     var img = document.getElementById('imageHolder');
     img.src = "shitPilot.gif"
     img.style.display = 'block';
+} else {
+    var nopeVid = document.getElementById("NopeVidHolder");
+    nopeVid.style.display = 'block';
+    nopeVid.play();
+    var startAgain = document.getElementById("TryAgain");
+    
+    startAgain.style.display = 'block';
+
+}
+    
 }
